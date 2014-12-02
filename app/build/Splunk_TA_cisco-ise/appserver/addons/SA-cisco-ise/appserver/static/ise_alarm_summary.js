84c7589fc1cfdff46ed1d4cdb555275d2f7068fa
JasonConger <jason@jasonconger.com>
2014-12-02 16:57:07 -0600

    Removed TA

--- app/build/Splunk_TA_cisco-ise/appserver/addons/SA-cisco-ise/appserver/static/ise_alarm_summary.js
+++
@@ -1,7 +0,0 @@
-require(['jquery'], function($){
-    
-    $(function() {
-	$('#field1').prependTo($('#tblAlarmSummary .panel-body')).find('label').hide();
-	$('#field3').prependTo($('#table1 .panel-body')).find('label').hide();
-    });
-});
